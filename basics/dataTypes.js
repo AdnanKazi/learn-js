93c837a940b0addb9696d9157a657a4f78414326
Adnan Kazi <adnan.kazi75@gmail.com>
2025-09-24 06:18:06 +0000

    Add initial JavaScript files and devcontainer configuration

---
+++ basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser 
+
+console.log(2+3) 
+console.log("Adnan")
+
+let anme  = "Adnan"
+let age =23
+let isLoggedIn = false
+
+// number  => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+// object => 
+
+console.log(typeof isLoggedIn)
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined 
---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 144223
+let accountEmail = "adnan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+let accountState;
+
+// accounId = 2 not allowed 
+
+// accountId = 2
+accountEmail = "something"
+accountPassword = '1123123'
+accountCity = 'dasdasd'
+accountState = 'asd'
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState])
